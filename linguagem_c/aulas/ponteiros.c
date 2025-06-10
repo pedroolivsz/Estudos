00bbc56581eab3bb0415ca5fd48a12216bffcb13
Pedro Oliveira <joaopedropereira2209@gmail.com>
2025-06-10 09:26:00 -0300

    Atualização do arquivo de ponteiros

--- linguagem_c/aulas/ponteiros.c
+++ linguagem_c/aulas/ponteiros.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main() {
+    int *p, x=10;
+    p = &x;
+
+    printf("*p: %d\n", *p);
+    printf("p: %d\n", p);
+    printf("x: %d\n", x);
+    printf("&x: %d\n", &x);
+}
